66f5db7efb0a512b962e55a2f42bd9babc39a732
Matan Maron <matanmaron@gmail.com>
2020-06-02 16:13:37 +0300

    stage 2 of remaster

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI MusicText = null;
+    [SerializeField] TextMeshProUGUI SFXText = null;
+    [SerializeField] GameObject MenuPanel = null;
+    [SerializeField] GameObject SettingsPanel = null;
+
+    private void Start()
+    {
+        MenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        if (Settings.MuteMusic)
+        {
+            MusicText.text = "Music: OFF";
+        }
+        else
+        {
+            MusicText.text = "Music: ON";
+        }
+        if (Settings.MuteSFX)
+        {
+            SFXText.text = "SFX: OFF";
+        }
+        else
+        {
+            SFXText.text = "SFX: ON";
+        }
+    }
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnAboutButton()
+    {
+        Application.OpenURL("https://matanmaron.wixsite.com/home/about");
+    }
+
+    public void OnMusic()
+    {
+        if (Settings.MuteMusic)
+        {
+            Settings.MuteMusic = false;
+            MusicText.text = "Music: ON";
+        }
+        else
+        {
+            Settings.MuteMusic = true;
+            MusicText.text = "Music: OFF";
+        }
+    }
+
+    public void OnSFX()
+    {
+        if (Settings.MuteSFX)
+        {
+            Settings.MuteSFX = false;
+            SFXText.text = "SFX: ON";
+        }
+        else
+        {
+            Settings.MuteSFX = true;
+            SFXText.text = "SFX: OFF";
+        }
+    }
+
+    public void OnSettings()
+    {
+        MenuPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void OnSettingsBack()
+    {
+        MenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -1,107 +1,52 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PlayerControll : MonoBehaviour
 {
-    [SerializeField] float Speed = 15;
-    [SerializeField] float sensitivity = 10;
-    [SerializeField] bool invert = false;
     [SerializeField] GameObject CameraHolder = null;
-
-    bool pickuping = false;
-    bool walking = false;
-    float player_rotationX;
-    float camera_minimumY = -20;
-    float camera_maximumY = 40;
+    [SerializeField] float Speed = 15;
+    float camera_minimumY = -10;
+    float camera_maximumY = 50;
+    float player_rotationX = 0;
     float camera_rotationY = 0;
     float camera_rotationX = 0;
-    Animator anim = null;
 
-    void Start()
+    private void Start()
     {
-        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void LateUpdate()
+    private void Update()
     {
-        if (!GameManager.Instance.GamePaused)
-        {
-            MovePlayer();
-            MoveCamera();
-        }
+        MovePlayer();
+        MoveCamera();
     }
 
     void MovePlayer()
     {
-        var inputX = Input.GetAxis("Horizontal");
         var inputY = Input.GetAxis("Vertical");
-        WalkAnim();
-        transform.position += transform.right * Speed * inputX * Time.deltaTime;
+        var inputX = Input.GetAxis("Horizontal");
         transform.position += transform.forward * Speed * inputY * Time.deltaTime;
-        player_rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        transform.position += transform.right * Speed * inputX * Time.deltaTime;
+        player_rotationX += Input.GetAxis("Mouse X") * Settings.MouseSensitivity;
         transform.localEulerAngles = new Vector3(0, player_rotationX, 0);
+
     }
 
     void MoveCamera()
     {
-        if (invert)
+        if (Settings.Invert)
         {
-            camera_rotationY -= Input.GetAxis("Mouse Y") * sensitivity;
+            camera_rotationY -= Input.GetAxis("Mouse Y") * Settings.MouseSensitivity;
         }
         else
         {
-            camera_rotationY += Input.GetAxis("Mouse Y") * sensitivity;    
+            camera_rotationY += Input.GetAxis("Mouse Y") * Settings.MouseSensitivity;
         }
-        camera_rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        camera_rotationX += Input.GetAxis("Mouse X") * Settings.MouseSensitivity;
         camera_rotationY = Mathf.Clamp(camera_rotationY, camera_minimumY, camera_maximumY);
         CameraHolder.transform.localEulerAngles = new Vector3(-camera_rotationY, camera_rotationX, 0);
         CameraHolder.transform.position = transform.position;
     }
-    
-    private void WalkAnim()
-    {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)
-            ||Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (!pickuping)
-            {
-                walking = true;
-                anim.Play("walk");
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)
-            || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            if (!pickuping)
-            {
-                walking = false;
-                anim.Play("idle");
-            }
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        GameManager.Instance.Hit(collision);
-    }
-
-    private void PickUpAnim()
-    {
-        pickuping = true;
-        anim.Play("pickup");
-        Invoke("PickUpAnimOff", 1);
-    }
-
-    void PickUpAnimOff()
-    {
-        pickuping = false;
-        if (walking)
-        {
-            anim.Play("walk");
-        }
-    }
-
 }
