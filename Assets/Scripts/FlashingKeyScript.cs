7bce295271c8ffe9c4f23e2d29b004341a886959
Matan Maron <matanmaron@gmail.com>
2020-06-03 17:16:10 +0300

    more level design

---
+++ Assets/Scripts/FlashingKeyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingKeyScript : MonoBehaviour
+{
+    [SerializeField] Color colorEnd = Color.white;
+    Color colorStart = Color.white;
+    float duration = 1.0f;
+    Renderer rend = null;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+    }
+}
